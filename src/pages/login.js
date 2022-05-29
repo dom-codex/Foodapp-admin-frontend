52bf75b2b39419f573db5b4bcb1ecd77f893d4d9
dom-codex <dominicwest7@gmail.com>
2022-05-29 16:59:53 +0100

    design login

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,25 +3,48 @@ import { getDustBg } from '../asset/dustsvg.js';
 const Login = () => {
   return (
     <section className={`mainbg h-screen`}>
-      <div className={'h-full flex'}>
-        <div>
+      <div className={'h-full w-full flex justify-center'}>
+        <div
+          className={
+            'bg-[rgba(0,0,0,0.01)] flex items-center backdrop-blur-sm flex-col mt-8 p-11 w-3/4 h-min rounded-md overflow-hidden shadow-lg'
+          }
+        >
           <div>
             <img src={''} />
           </div>
-          <h2>FOODIE Admin</h2>
-          <div>
-            <div>
-              <i className={'materal-icons'}></i>
-              <input placeholder={'Email or staffId'} type={'text'} />
+          <h2 className={'text-[2rem] font-bold'}>FOODIE </h2>
+          <p>Admin login</p>
+          <div className={'mt-8'}>
+            <div className={' mb-4 h-12 relative rounded-md overflow-hidden'}>
+              <i className={'material-icons absolute top-3 left-2'}>mail</i>
+              <input
+                placeholder={'Email or staffId'}
+                type={'text'}
+                className={'h-full bg-[rgba(0,0,0,0.05)] pl-8'}
+              />
             </div>
-            <div>
-              <i className={'material-icons'}></i>
-              <input type={'password'} placeholder={'password'} />
+            <div
+              className={
+                ' mb-4 h-12 reltive rounded-md overflow-hidden relative'
+              }
+            >
+              <i className={'material-icons absolute top-3 left-2'}>lock</i>
+              <input
+                type={'password'}
+                placeholder={'password'}
+                className={'h-full bg-[rgba(0,0,0,0.05)] pl-8'}
+              />
             </div>
-            <div>
-              <button>Sign in</button>
+            <div className={'w-3/4 mx-auto h-11 rounded-md overflow-hidden'}>
+              <button
+                className={
+                  'h-full bg-black text-white w-full hover:bg-black/50 hover:rounded-lg hover:font-bold hover:text-black border-4 border-black'
+                }
+              >
+                Sign in
+              </button>
             </div>
-            <div>
+            <div className={'text-right mt-4'}>
               <small>forgot password?</small>
             </div>
           </div>
