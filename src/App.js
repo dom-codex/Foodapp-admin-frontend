77b80d2aef0b5483d70d8360e549235d41ca0766
dom-codex <dominicwest7@gmail.com>
2022-05-29 01:48:22 +0100

    Add tailwind play CDN

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 export default function App() {
   return (
-    <div>
+    <div className="text-red-50">
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
