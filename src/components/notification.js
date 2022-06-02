90a51f2ada7897c232b6125832846adc5b76e7a5
dom-codex <dominicwest7@gmail.com>
2022-06-02 06:43:52 +0100

    Update notification padding

--- src/components/notification.js
+++ src/components/notification.js
@@ -1,25 +1,37 @@
-import React from "react"
-const Notification =()=>{
-return <div className="w-[95%] mx-auto  mt-4 rounded-md bg-black/5 backdrop-blur-sm">
-  <div className="flex justify-between items-center  px-2 h-[48px]">
-    <p className="font-bold relative  
-    after:absolute after:top-[10px] after:-right-[10px] after:h-[6px] after:w-[6px] after:bg-black after:content-[''] after:rounded-md">Notifications</p>
-    <button className="text-sm text-gray-700">mark all as read</button>
-  </div>
-  <div className="p-2">
-  <div className="flex items-center">
-    <i className ={"material-icons icon "}>notifications</i>
-    <div>
-      <h3 className="font-bold">New complaint Email</h3>
-      <div className="text-gray-500">
-        <span>sender:</span>
-        <button className="text-gray-700 underline">dominic@gmail.com</button>
+import React from 'react';
+const Notification = () => {
+  return (
+    <div className="w-[95%] mx-auto  mt-4 rounded-md bg-black/5 backdrop-blur-sm">
+      <div className="flex justify-between items-center  px-2 h-[48px]">
+        <p
+          className="font-bold relative  
+    after:absolute after:top-[10px] after:-right-[10px] after:h-[6px] after:w-[6px] after:bg-black after:content-[''] after:rounded-md"
+        >
+          Notifications
+        </p>
+        <button className="text-sm text-gray-700">mark all as read</button>
       </div>
-      <small className="text-gray-700">Mon, jan 29 2023,2:30am </small>
+      <div className="p-2">
+        <div className="flex items-center">
+          <i className={'material-icons icon '}>notifications</i>
+          <div>
+            <h3 className="font-bold">New complaint Email</h3>
+            <div className="text-gray-500">
+              <span>sender:</span>
+              <button className="text-gray-700 underline">
+                dominic@gmail.com
+              </button>
+            </div>
+            <small className="text-gray-700">Mon, jan 29 2023,2:30am </small>
+          </div>
+        </div>
+      </div>
+      <div className="text-center bg-black/7 backdrop-blur-sm  mt-2 p-3">
+        <a href="#" className="hover:underline">
+          view more
+        </a>
+      </div>
     </div>
-  </div>
-  </div>
-  <div className="text-center bg-black/7 backdrop-blur-sm  mt-2 p-2"><a href="#" className="hover:underline">view more</a></div>
-</div>
-}
-export default Notification
+  );
+};
+export default Notification;
