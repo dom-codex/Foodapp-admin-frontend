e8fa692b895e6ee99f2dd5bad613496f8bb4182e
dom-codex <dominicwest7@gmail.com>
2022-06-04 02:39:29 +0100

    Design meal card

--- src/components/mealcard.js
+++ src/components/mealcard.js
@@ -1,21 +1,21 @@
 import React from "react"
-
+import {getFoodImageUrl} from "../asset/getimages.js"
 const MealCard=()=>{
-return <div>
-  <div>
-<img src={"../asset/jollofrice.jpg"}/>
-<button><i className="material-icons">edit</i></button>
+return <div className="rounded-md shadow-md bg-white/10 backdrop-blur-sm border-[1px] border-slate-100 w-[95%] mx-auto mt-4">
+  <div className="relative overflow-hidden max-h-[180px]">
+<img src={getFoodImageUrl()} className="w-fit h-fit"/>
+<button className="absolute bottom-[6px] right-[8px] bg-black/40 shadow-sm rounded-sm p-[8px]"><i className="material-icons text-slate-200">edit</i></button>
   </div>
-  <div>
-    <h2>Jollof rice </h2>
-    <div>
+  <div className="m-4">
+    <h2 className="font-bold ">Jollof rice </h2>
+    <div className="flex">
       <p>Product Id: </p>
       <p>200701</p>
     </div>
     <p>N100,000</p>
-    <div>
-      <p>rating</p>
-      <p>4.3</p>
+    <div className="flex">
+      <p>rating: </p>
+      <p className="">4.3</p>
     </div>
   </div>
 </div>
