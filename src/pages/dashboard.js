befffb4a9aa2f20975471afb8f97447e046920f3
dom-codex <dominicwest7@gmail.com>
2022-06-04 14:37:24 +0100

    Add greetings to dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -7,8 +7,19 @@ const Dashboard = () => {
   return (
     <section>
       <div>
+        <div className={"flex w-[95%] mx-auto mb-8"}>
+          <div>
+            <h2 className="font-bold mb-0">Welcome Dominic!!!</h2>
+            <small className="text-slate-900 mt-0">
+              <span>access level:</span>
+              <span>Owner</span>
+            </small>
+          </div>
+        </div>
         {/**stats section  */}
+        <div>
         <Statscard />
+        </div>
         <MealCard/>
         <Notification />
         <Activity/>
