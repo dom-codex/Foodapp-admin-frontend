03b0954ec8baa0eb1d7ba3c6d2af0a2ef2946601
dom-codex <dominicwest7@gmail.com>
2022-06-07 22:00:07 +0100

    Improve design

--- src/pages/foods.js
+++ src/pages/foods.js
@@ -5,7 +5,17 @@ const Foods =()=>{
   const foods = [{},{},{}]
   const categories = [{},{},{},{},{}]
 return <section className="pb-4">
-  <div>
+  <div className="w-[95%] mx-auto">
+    <div className="mb-4">
+      <div>
+      <h2 className="text-[1.65rem] font-bold">Menu</h2>
+      {/**floating menu */}
+      </div>
+      <div className="text-slate-600">
+        <span>Total: </span>
+        <span>52 items</span>
+      </div>
+    </div>
     <div className="overflow-auto whitespace-nowrap mb-6">
       {categories.map(()=><Category/>)}
     </div>
