e4dc9311f655b3e293a1fc3ef1c0c6061f6f2510
dom-codex <dominicwest7@gmail.com>
2022-06-07 18:27:55 +0100

    Redesign category item

--- src/components/category.js
+++ src/components/category.js
@@ -1,9 +1,9 @@
 import React from "react"
 
 const Category =()=>{
-return <div className="inline-flex items-center bg-slate-500/50 rounded-md  p-2 min-w-[100px]">
+return <div className="inline-flex items-center bg-red-400/30 rounded-md  p-2 min-w-[100px]">
   <div className="h-[8px] w-[8px] rounded-lg bg-red-500 mr-1"></div>
-  <p className="inline text-white font-bold">Local</p>
+  <p className="inline text-slate-400 font-bold">Local</p>
 </div>
 }
 export default Category
