f11d3a7df45dd45371d50c64941e3b4e2e2e1ebe
dom-codex <dominicwest7@gmail.com>
2022-06-07 21:31:02 +0100

    Add horizontal scroll for category

--- src/components/category.js
+++ src/components/category.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Category =()=>{
-return <div className="inline-flex items-center bg-red-400/30 rounded-md  p-2 min-w-[100px]">
+return <div className="inline-flex items-center bg-red-400/30 rounded-md  p-2 min-w-[100px] mx-2">
   <div className="h-[8px] w-[8px] rounded-lg bg-red-500 mr-1"></div>
   <p className="inline text-slate-400 font-bold">Local</p>
 </div>
--- src/pages/foods.js
+++ src/pages/foods.js
@@ -3,10 +3,11 @@ import Category from "../components/category.js"
 import MealCard from "../components/mealcard.js"
 const Foods =()=>{
   const foods = [{},{},{}]
+  const categories = [{},{},{},{},{}]
 return <section className="pb-4">
   <div>
-    <div>
-      <Category/>
+    <div className="overflow-auto whitespace-nowrap mb-6">
+      {categories.map(()=><Category/>)}
     </div>
     <div className={"grid place-items-center"}>
       {foods.map(()=><MealCard/>)}
