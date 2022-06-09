7222b5ba27fe9b7523f63772321054b8c7d49b19
dom-codex <dominicwest7@gmail.com>
2022-06-09 21:59:36 +0100

    Add customers page

---
+++ src/pages/customers.js
@@ -0,0 +1,22 @@
+import React from "react"
+import SearchBar from "../components/searchbar.js"
+const Customers =()=>{
+return <section>
+  <div>
+    <div>
+      <div>
+        <h2>Customers</h2>
+        <div>
+          <span>Total</span>
+          <small>300</small>
+        </div>
+      </div>
+      <div>
+        <SearchBar/>
+      </div>
+    </div>
+    <div></div>
+    </div>
+</section>
+}
+export default Customers
