b75e0088daed25a6309eaf4841ac9fd0cde4f56f
dom-codex <dominicwest7@gmail.com>
2022-06-09 17:20:06 +0100

    Implement responsive design partially

--- src/components/category.js
+++ src/components/category.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Category =()=>{
-return <div className="inline-flex items-center bg-red-400/30 rounded-md  p-2 min-w-[100px] mr-2">
+return <div className="inline-flex items-center bg-red-400/30 rounded-md  p-2 min-w-[100px] mr-2 sm:flex sm:mb-2">
   <div className="h-[8px] w-[8px] rounded-lg bg-red-500 mr-1"></div>
   <p className="inline text-slate-600 font-bold">Local</p>
 </div>
--- src/pages/foods.js
+++ src/pages/foods.js
@@ -1,42 +1,53 @@
-import React from "react"
-import Category from "../components/category.js"
-import MealCard from "../components/mealcard.js"
-import FloatingMenu from "../components/floatingmenu.js"
-const Foods =()=>{
-  const foods = [{},{},{}]
-  const categories = [{},{},{},{},{}]
-return <section className="pb-4">
-  <div className="w-[95%] mx-auto">
-    <div className="mb-4">
-      <div className="flex justify-between">
-      <h2 className="text-[1.65rem] font-bold">Menu</h2>
-      {/**floating menu */}
-      <div  className="relative group ">
-      <button className="border-2 border-slate-500 text-slate-600 rounded-md bg-white flex items-center justify-center  w-[90px] p-2 ">
-        <i className="material-icons ">
-          add
-        </i>
-        <span>Add</span>
-      </button>
-        <FloatingMenu>
-          <li className="p-2 hover:bg-slate-100">category</li>
-          <li className="h-[1px] bg-slate-200 "></li>
-          <li className="p-2 hover:bg-slate-100">Food</li>
-          </FloatingMenu>
+import React from 'react';
+import Category from '../components/category.js';
+import MealCard from '../components/mealcard.js';
+import FloatingMenu from '../components/floatingmenu.js';
+const Foods = () => {
+  const foods = [{}, {}, {}];
+  const categories = [{}, {}, {}, {}, {}];
+  return (
+    <section className="pb-4">
+      <div className="w-[95%] mx-auto">
+        <div className="mb-4">
+          <div className="flex justify-between">
+            <h2 className="text-[1.65rem] font-bold">Menu</h2>
+            {/**floating menu */}
+            <div className="relative group ">
+              <button className="border-2 border-slate-500 text-slate-600 rounded-md bg-white flex items-center justify-center  w-[90px] p-2 ">
+                <i className="material-icons ">add</i>
+                <span>Add</span>
+              </button>
+              <FloatingMenu>
+                <li className="p-2 hover:bg-slate-100">category</li>
+                <li className="h-[1px] bg-slate-200 "></li>
+                <li className="p-2 hover:bg-slate-100">Food</li>
+              </FloatingMenu>
+            </div>
+          </div>
+          <div className="text-slate-600">
+            <span>Total: </span>
+            <span>52 items</span>
+          </div>
+        </div>
+        <div className="sm:flex sm:overflow-y-scroll ">
+          <div
+            className="overflow-auto whitespace-nowrap mb-6 sm:w-[280px] sm:whitespace-normal sm:overflo
+   "
+          >
+            {categories.map(() => (
+              <Category />
+            ))}
+          </div>
+          <div
+            className={'grid place-items-center sm:flex-1 sm:overflow-y-scroll'}
+          >
+            {foods.map(() => (
+              <MealCard />
+            ))}
+          </div>
+        </div>
       </div>
-      </div>
-      <div className="text-slate-600">
-        <span>Total: </span>
-        <span>52 items</span>
-      </div>
-    </div>
-    <div className="overflow-auto whitespace-nowrap mb-6">
-      {categories.map(()=><Category/>)}
-    </div>
-    <div className={"grid place-items-center"}>
-      {foods.map(()=><MealCard/>)}
-    </div>
-  </div>
-</section>
-}
-export default Foods
+    </section>
+  );
+};
+export default Foods;
