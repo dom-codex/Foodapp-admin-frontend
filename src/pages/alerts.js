20375d1049c65250a0965b2699c83cf882a2d6ec
dom-codex <dominicwest7@gmail.com>
2022-06-11 20:52:30 +0100

    add transition effect to alert tab toggler

--- src/pages/alerts.js
+++ src/pages/alerts.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageLabel from '../components/pagelabel.js';
 const Alerts = () => {
+  const [tabToggle, toggler] = useState(true);
   return (
     <section>
       <div>
         <PageLabel label="Alerts" value="200" />
         <div>
-          <Tab isNotification={false} />
+          <Tab isNotification={tabToggle} toggler={toggler} />
         </div>
       </div>
     </section>
   );
 };
 export default Alerts;
-const Tab = ({ isNotification }) => {
+const Tab = ({ isNotification, toggler }) => {
   const indicatorRight = 'before:translate-x-[100%]';
   const activeBg = 'bg-red-100';
+  const setToggler = (value) => {
+    toggler(value);
+  };
   return (
     <div>
       <div
         className={`flex relative before:absolute before:left-0 before:bottom-0 before:w-1/2 before:border-t-2 before:border-red-500
         before:translate-x-[0%]
-        transition transition-1 ${!isNotification && indicatorRight}`}
+        before:transition before:duration-500 ${
+          !isNotification && indicatorRight
+        }`}
       >
-        <button className={`flex-1 transition-1 ${isNotification && activeBg}`}>
+        <button
+          onClick={() => setToggler(true)}
+          className={`flex-1 transition duration-500 py-4 ${
+            isNotification && activeBg
+          }`}
+        >
           Notifications
         </button>
         <button
-          className={`flex-1 transition transition-1 ${
+          onClick={() => setToggler(false)}
+          className={`flex-1 transition duration-500 py-4 ${
             !isNotification && activeBg
           }`}
         >
