355945e0cfae90176eabbad1af9bf7a845738853
dom-codex <dominicwest7@gmail.com>
2022-06-11 15:55:26 +0100

    add rider component sturcture

---
+++ src/components/rider.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Rider = () => {
+  return (
+    <div>
+      <div>
+        <img src={''} alt={'rider picture'} />
+        <div>
+          <small>Rider Id:</small>
+          <p>0021456</p>
+        </div>
+      </div>
+      <div>
+        <p>DOMINIC WEST</p>
+        <p>DOM@gmail.com</p>
+        <p>08101063084</p>
+      </div>
+      <div>
+        <p>Total Deliveries</p>
+        <p>200s</p>
+      </div>
+      <div>
+        <button>View Location</button>
+      </div>
+    </div>
+  );
+};
+export default Rider;
--- src/pages/riders.js
+++ src/pages/riders.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PageLabel from '../components/pagelabel.js';
+import Rider from '../components/rider.js';
 const Riders = () => {
   return (
     <section>
       <div>
         <PageLabel label="Riders" value="01" />
+        <div>
+          <Rider />
+        </div>
       </div>
     </section>
   );
