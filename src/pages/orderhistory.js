6b15060d1f5078e7332da2c07c94ee629a1796bb
dom-codex <dominicwest7@gmail.com>
2022-06-13 06:47:17 -0700

    add order item

--- src/pages/orderhistory.js
+++ src/pages/orderhistory.js
@@ -7,15 +7,53 @@ const OrderHistory = () => {
         <PageLabel label="OrderHistory" value={24} />
         <div>
           <OrderHeader />
+          <OrderBody />
         </div>
       </div>
     </section>
   );
 };
 export default OrderHistory;
+const OrderItem = () => {
+  return (
+    <div className="grid grid-cols-[repeat(6,1fr)]">
+      <div>
+        <p>000001</p>
+      </div>
+      <div>
+        <p>13/06/2022</p>
+      </div>
+      <div>
+        <p>dom@gmail.com</p>
+      </div>
+      <div>
+        <p>$200.56</p>
+      </div>
+      <div>
+        <p>Processing</p>
+      </div>
+      <div className="flex items-center">
+        <button>View order</button>
+        <button className="ml-6">
+          <i className="material-icons">clear</i>
+        </button>
+      </div>
+    </div>
+  );
+};
+const OrderBody = () => {
+  const orders = [{}, {}, {}];
+  return (
+    <div className="mt-3">
+      {orders.map(() => (
+        <OrderItem />
+      ))}
+    </div>
+  );
+};
 const OrderHeader = () => {
   return (
-    <div className="grid grid-cols-[repeat(6,1fr) ">
+    <div className="grid grid-cols-[repeat(6,1fr)] ">
       <div>
         <p>Order #</p>
       </div>
